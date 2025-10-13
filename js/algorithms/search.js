e6ea0a13ec27cdb33fb97e892d065f88df6536bf
Hasodya Pandigamage <gahdse242f-012@student.nibm.lk>
2025-10-13 11:23:31 +0530

    search alg snack position

--- js/algorithms/search.js
+++ js/algorithms/search.js
@@ -16,5 +16,27 @@ const search = {
 
         return -1;
     },
-    
+
+    findSpecialTile(position, specialTiles) {
+        // O(1) lookup using object as hash map
+        return specialTiles[position] || null;
+    },
+
+    findNearestLadder(position, ladders) {
+        const ladderStarts = Object.keys(ladders)
+            .map(Number)
+            .filter(start => start > position)
+            .sort((a, b) => a - b);
+            
+        return ladderStarts[0] || null;
+    },
+
+    findNearestSnake(position, snakes) {
+        const snakeStarts = Object.keys(snakes)
+            .map(Number)
+            .filter(start => start > position)
+            .sort((a, b) => a - b);
+            
+        return snakeStarts[0] || null;
+    }
 };
