fd2d120736945310feb2e3da6d310cecd592f124
Hasodya Pandigamage <gahdse242f-012@student.nibm.lk>
2025-10-13 11:36:06 +0530

    animation/processQueue

--- js/animations.js
+++ js/animations.js
@@ -6,4 +6,23 @@ const animations = {
         this.animationQueue.push(action);
         if (!this.isAnimating) this.processQueue();
     },
+
+    processQueue() {
+        if (this.animationQueue.length === 0) {
+            this.isAnimating = false;
+            window.game.rollDiceBtn.disabled = !gameLogic.gameInProgress;
+            return;
+        }
+        
+        this.isAnimating = true;
+        window.game.rollDiceBtn.disabled = true;
+        const nextAction = this.animationQueue.shift();
+        const result = nextAction();
+        
+        if (result instanceof Promise) {
+            result.then(() => this.processQueue());
+        } else {
+            this.processQueue();
+        }
+    },
 };
